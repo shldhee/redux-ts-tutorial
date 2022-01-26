d05bfe9b73518f94d5f23f7a67e41ad7d5fc37a9
shldhee <shldhee@yahoo.co.kr>
2022-01-26 21:19:02 +0900

    chore: redux-thunk 의존성 설치

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const middleware = [logger]
+const middleware = [logger, thunk]
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middleware))
